f1979865338ff0a3bde1ff5a67076fecb6c897ee
codeckz <ragasadan@outlook.com>
2025-08-15 15:47:41 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
   document.addEventListener("DOMContentLoaded", function () {
-    const targetUrl = "/men";
+    const targetUrl = "https://barakogym.com/products/collections/woman";
     const currentUrl = window.location.href;
 
     if (currentUrl.includes (targetUrl)) {
